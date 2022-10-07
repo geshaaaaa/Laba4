e532ea16e90675f3b27bb483e87c7645c50063dc
gesha <genabro@ukr.net>
2022-10-07 19:10:25 +0300

    finished

---
+++ ConsoleApp3/Croco.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavd1
+{ 
+    class Croco
+    {
+        protected int a = 10;       
+        protected string c = "parametr";
+        public virtual void Print()
+        {
+            Console.WriteLine($"Перший агрумент {a}");
+
+           
+            Console.WriteLine($"Третій агрумент {c}");
+        }
+
+        static void Main (string[] args)
+        {
+            Zebra zebra = new Zebra();
+            zebra.Print();
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+}
---
+++ ConsoleApp3/Zebra.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavd1
+{
+    class Zebra : Croco
+    {
+        public override void Print()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Перший агрумент: {a} \n {a.GetType}");          
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"Третій агрумент: {c} \n {c.GetType}");
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+
+    }
+}
